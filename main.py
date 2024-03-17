5c6d502f8ee9ca4f2fec2b8d4df8024b531ddb64
VITUSEVICH <vladzero5559@gmail.com>
2024-03-17 21:51:27 +0300

    Второй

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-testing
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def display_balance(self):
+        print("Баланс счета:", self.balance)
+
+
+account = BankAccount(1000)
+account.display_balance()
+account.deposit(500)
+account.display_balance()
+account.withdraw(200)
+account.display_balance()
+
 
 
 
